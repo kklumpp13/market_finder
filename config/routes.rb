f8807cd875330c02bd137202cebe346cd9327c51
Krystal Klumpp <kklumpp13@gmail.com>
2015-06-04 16:22:28 -0400

    Added basic user authentificaiton

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  # devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users
   #go to the welcome controller and the index action
   root 'welcome#index'
 
