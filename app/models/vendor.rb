a548a629b91b24e0161f8818d69a5e847f4f6a69
Krystal Klumpp <kklumpp13@gmail.com>
2015-06-14 15:44:33 -0400

    Add AP to Gemfile. Fixed slugs.

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,4 +4,7 @@ class Vendor < ActiveRecord::Base
   has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }  
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
 end
